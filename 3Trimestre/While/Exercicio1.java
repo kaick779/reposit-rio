aa776021bd2c3134c548f060136eb3d50b8b2dba
kaick779 <kaickmendes2007@gmail.com>
2024-09-24 19:46:24 -0300

    Add files via upload

---
+++ 3Trimestre/While/Exercicio1.java
@@ -0,0 +1,14 @@
+
+public class Exercicio1 {
+	public static void main(String Args[]) {
+
+		
+		int i=0;
+		int r = 0;
+		while (i<=100) {
+			r += i;
+			i++;
+		}
+		System.out.println(r);
+	}
+}
